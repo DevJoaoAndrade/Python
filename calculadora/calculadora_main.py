909a405df2ff65d81de3f014d56c218953e2bac8
João Francisco Bispo de Andrade <dev.joaoandrade@gmail.com>
2025-03-29 17:28:20 -0300

    Adicionando a operação de divisão e modificando o menu

--- calculadora/calculadora_main.py
+++ calculadora/calculadora_main.py
@@ -14,25 +14,32 @@ while True:
     elif operador in "+-*/":
         
         if flag_total:
-            total = int(input('Digite um valor: '))
+            total = int(input(f'Digite um número: '))
             flag_total = False
         
         while True:
-            segundo_numero = int(input('Digite outro numero: '))
+            segundo_numero = int(input(f'{total} {operador} '))
             
             if operador == '+':
                 total = calculos.soma(total, segundo_numero)
-                break
+                break 
             elif operador == '-':
-                total = calculos.subtracao(total, segundo_numero)
-                break
+                total = calculos.subtracao(total, segundo_numero)  
+                break  
             elif operador == '*':
                 total = calculos.multiplicacao(total, segundo_numero)
-                break
+                break   
             elif operador == '/':
-                pass
-            else:
-                print('Operador inválido!')
+                try:
+                    total = calculos.divisao(total, segundo_numero)
+                    break
+                except Exception as erro_divisao_zero:
+                    print('Erro por divisão por zero. Reiniciando operação.')
+                    total = 0 if total > 0 else total
+                    flag_total = True
+                    break
+    else:
+        print('Operador inválido!')
 
     
     
