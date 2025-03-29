e1d3f3af35b69cc52362394134d142f8016f2cba
João Francisco Bispo de Andrade <dev.joaoandrade@gmail.com>
2025-03-29 17:28:50 -0300

    Criando a função de divisao

--- calculadora/funcoes/calculos.py
+++ calculadora/funcoes/calculos.py
@@ -10,6 +10,6 @@ def multiplicacao(numero1, numero2):
     return numero1 * numero2
 
 
-def soma(numero1, numero2):
-    return numero1 + numero2
+def divisao(numero1, numero2):
+    return numero1 / numero2
 
