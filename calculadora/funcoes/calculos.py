be2f739895e4b0a8f1f351b48d1bd3f104e4e41b
João Francisco Bispo de Andrade <dev.joaoandrade@gmail.com>
2025-03-29 12:23:27 -0300

    Adicionando as funções de subtração. multiplicação e divis

--- calculadora/funcoes/calculos.py
+++ calculadora/funcoes/calculos.py
@@ -1,2 +1,15 @@
 def soma(numero1, numero2):
     return numero1 + numero2
+
+
+def subtracao(numero1, numero2):
+    return numero1 - numero2
+
+
+def multiplicacao(numero1, numero2):
+    return numero1 * numero2
+
+
+def soma(numero1, numero2):
+    return numero1 + numero2
+
