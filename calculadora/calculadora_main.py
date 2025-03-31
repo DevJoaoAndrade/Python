ca3f0ffecb46826c018d3f6e16408ce443aaa7c9
João Francisco Bispo de Andrade <dev.joaoandrade@gmail.com>
2025-03-31 17:12:32 -0300

    Mudando a nomeclaura do pacote e adicionando o __init__

--- calculadora/calculadora_main.py
+++ calculadora/calculadora_main.py
@@ -1,4 +1,4 @@
-from funcoes import calculos, validador
+from pacote import modulo_calculos, modulo_validador
 
 total = 0
 flag_primeiro_numero = True
@@ -17,7 +17,7 @@ while True:
         if flag_primeiro_numero:
             primeiro_numero = input(f'Digite um número: ')
             # Possibilidade cast recebe o input do usuário e se caso o retorno for True, podemos converter o valor para int.
-            possibilidade_cast = validador.verifica_numero(primeiro_numero)
+            possibilidade_cast = modulo_validador.verifica_numero(primeiro_numero)
             if possibilidade_cast:
                 total = int(primeiro_numero)
                 flag_primeiro_numero = False
@@ -28,7 +28,7 @@ while True:
         
         # Funciona da mesma forma do primeiro número com a diferença que esse sempre vai ser chamado caso estiver tudo certo com o primeiro número
         segundo_numero = input(f'{total} {operador} ')
-        possibilidade_cast = validador.verifica_numero(segundo_numero)
+        possibilidade_cast = modulo_validador.verifica_numero(segundo_numero)
         if possibilidade_cast:
             cast_segundo_numero = int(segundo_numero)
         else:
@@ -36,14 +36,14 @@ while True:
         
         while True:
             if operador == '+':
-                total = calculos.soma(total, cast_segundo_numero)
+                total = modulo_calculos.soma(total, cast_segundo_numero)
             elif operador == '-':
-                total = calculos.subtracao(total, cast_segundo_numero)      
+                total = modulo_calculos.subtracao(total, cast_segundo_numero)      
             elif operador == '*':
-                total = calculos.multiplicacao(total, cast_segundo_numero) 
+                total = modulo_calculos.multiplicacao(total, cast_segundo_numero) 
             elif operador == '/':
                 try:
-                    total = calculos.divisao(total, cast_segundo_numero)
+                    total = modulo_calculos.divisao(total, cast_segundo_numero)
                 except Exception as erro_divisao_zero:
                     print('Erro por divisão por zero. Reiniciando operação.')
                     total = 0 if total > 0 else total
