b0b62d7db717ad591ff0cc80be0c5561925de9a2
João Francisco Bispo de Andrade <dev.joaoandrade@gmail.com>
2025-03-31 11:33:32 -0300

    Adicionando um verificador de número para ver se os inputs são números e removendo breaks desnecessários

--- calculadora/calculadora_main.py
+++ calculadora/calculadora_main.py
@@ -1,7 +1,7 @@
 from funcoes import calculos, validador
 
 total = 0
-flag_total = True
+flag_primeiro_numero = True
 
 while True:
     
@@ -11,33 +11,44 @@ while True:
     if operador == '=':
         print(f'Total = {total}')
         break
+    
     elif operador in "+-*/":
+        # Essa flag é usada para pedir o primeiro número apenas 1 vez a cada inicio de operação.
+        if flag_primeiro_numero:
+            primeiro_numero = input(f'Digite um número: ')
+            # Possibilidade cast recebe o input do usuário e se caso o retorno for True, podemos converter o valor para int.
+            possibilidade_cast = validador.verifica_numero(primeiro_numero)
+            if possibilidade_cast:
+                total = int(primeiro_numero)
+                flag_primeiro_numero = False
+            # Caso contrário, ele apenas volta a flag do primeiro número para True e reinicia o calculo.
+            else:
+                flag_primeiro_numero = True
+                continue
         
-        if flag_total:
-            total = int(input(f'Digite um número: '))
-            flag_total = False
+        # Funciona da mesma forma do primeiro número com a diferença que esse sempre vai ser chamado caso estiver tudo certo com o primeiro número
+        segundo_numero = input(f'{total} {operador} ')
+        possibilidade_cast = validador.verifica_numero(segundo_numero)
+        if possibilidade_cast:
+            cast_segundo_numero = int(segundo_numero)
+        else:
+            continue
         
         while True:
-            segundo_numero = int(input(f'{total} {operador} '))
-            
             if operador == '+':
-                total = calculos.soma(total, segundo_numero)
-                break 
+                total = calculos.soma(total, cast_segundo_numero)
             elif operador == '-':
-                total = calculos.subtracao(total, segundo_numero)  
-                break  
+                total = calculos.subtracao(total, cast_segundo_numero)      
             elif operador == '*':
-                total = calculos.multiplicacao(total, segundo_numero)
-                break   
+                total = calculos.multiplicacao(total, cast_segundo_numero) 
             elif operador == '/':
                 try:
-                    total = calculos.divisao(total, segundo_numero)
-                    break
+                    total = calculos.divisao(total, cast_segundo_numero)
                 except Exception as erro_divisao_zero:
                     print('Erro por divisão por zero. Reiniciando operação.')
                     total = 0 if total > 0 else total
-                    flag_total = True
-                    break
+                    flag_primeiro_numero = True
+            break
     else:
         print('Operador inválido!')
 
