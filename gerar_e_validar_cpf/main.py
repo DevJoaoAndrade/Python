538041ec5ee7b74f1c75633710fb2c56f368e3b0
João Francisco Bispo de Andrade <dev.joaoandrade@gmail.com>
2025-04-02 18:27:17 -0300

    Criando o menu incial e implementando a primeira opção de gerar cpf.

--- gerar_e_validar_cpf/main.py
+++ gerar_e_validar_cpf/main.py
@@ -1,8 +1,30 @@
-from pacote_functions import modulo_gerar_digitos
+from utils.gerador_de_numeros import gerar_nove_numeros
+from utils.gerador_digito_verificador import gerar_digito_verificador
 
 
 if __name__ == '__main__':
-    nove_digitos = modulo_gerar_digitos.gerar_nove_digitos()
+    while True:
+        opcao_menu = input('1 - Gerar CPF\n2 - Verificar CPF\n3 - Sair\nDigite aqui: ')
+        if opcao_menu == '1':
+                
+            nove_digitos_cpf = gerar_nove_numeros()
+    
+            primeiro_verificador = gerar_digito_verificador(nove_digitos_cpf, 10)
+            dez_digitos_cpf = nove_digitos_cpf + primeiro_verificador
+    
+            segundo_verificador = gerar_digito_verificador(dez_digitos_cpf, 11)
+            cpf = dez_digitos_cpf + segundo_verificador
+            print(cpf)
+        elif opcao_menu == '2':
+            pass
+        elif opcao_menu == '3':
+            break
+        else:
+            print('Opção inválida, escolha outra opção.')
+    
+    
+    
+    
     
     
     
