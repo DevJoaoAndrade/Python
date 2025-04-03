387b17999e865d969f25f4ba62ffb684602425ec
João Francisco Bispo de Andrade <dev.joaoandrade@gmail.com>
2025-04-03 18:55:01 -0300

    Reduzindo a quantidade de código do meunu

--- gerar_e_validar_cpf/main.py
+++ gerar_e_validar_cpf/main.py
@@ -1,5 +1,7 @@
-from utils.gerador_de_numeros import gerar_nove_numeros, gerar_digito_verificador
+from utils.gerador_de_numeros import gerar_nove_numeros
 from utils.valida_numeros import removedor_de_simbolos
+from utils import iniciador_gera_validar
+
 import os
 import time
 
@@ -7,36 +9,21 @@ if __name__ == '__main__':
     while True:
         opcao_menu = input('1 - Gerar CPF\n2 - Validar CPF\n3 - Sair\nDigite aqui: ')
         if opcao_menu == '1':
-                
             nove_digitos_cpf = gerar_nove_numeros()
-    
-            primeiro_verificador = gerar_digito_verificador(nove_digitos_cpf, 10)
-            dez_digitos_cpf = nove_digitos_cpf + primeiro_verificador
-    
-            segundo_verificador = gerar_digito_verificador(dez_digitos_cpf, 11)
-            cpf = dez_digitos_cpf + segundo_verificador
+            cpf = iniciador_gera_validar(nove_digitos_cpf)
             print(cpf)
-        
         elif opcao_menu == '2':
             cpf_entrada = input('Digite o CPF: ')
             cpf_sem_simbolos = removedor_de_simbolos(cpf_entrada)
             nove_digitos_cpf = cpf_sem_simbolos[:9]
-            
             if nove_digitos_cpf.isdigit():
-                primeiro_verificador = gerar_digito_verificador(nove_digitos_cpf, 10)
-                dez_digitos_cpf = nove_digitos_cpf + primeiro_verificador
-                
-                segundo_verificador = gerar_digito_verificador(dez_digitos_cpf, 11)
-                cpf = dez_digitos_cpf + segundo_verificador
+                cpf = iniciador_gera_validar(nove_digitos_cpf)
                 if cpf_sem_simbolos == cpf:
                     print(f'CPF válido')
                 else:
                     print(f'CPF inválido')                
             else:
                 print('Ocorreu um erro, tente novamente.')
-                    
-            
-            
         elif opcao_menu == '3':
             break
         else:
